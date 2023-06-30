b44f0ec2c6e6eb104859425d8ce24f2dbea18e2d
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-30 06:12:55 +0000

    first

---
+++ src/Greeting.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function Greeting() {
+  const [name, setName] = useState('');
+  
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  
+  return (
+    <div>
+      <label htmlFor="nameInput">Enter your name: </label>
+      <input id="nameInput" type="text" value={name} onChange={handleNameChange} />
+      <p>{name ? `Hello, ${name}!` : 'Please enter your name.'}</p>
+    </div>
+  );
+}
+
+export default Greeting;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
+import Greeting from "./../Greeting";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Greeting/>
     </div>
   )
 }
